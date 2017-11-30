f7b9abb87a1e1a6e7f597716dcda6c8661ea51a9
George <georgesut@yahoo.com>
2017-11-30 23:27:43 +0200

    Add renderer

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #define WINDOW_WIDTH 700
 #define WINDOW_HEIGHT 500
@@ -7,6 +8,8 @@ int main() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("Stack Attack Reborn",
 										  0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
 	bool running = true;
 
 	if(window == NULL) {
@@ -14,13 +17,16 @@ int main() {
 		return 0;
 	}
 
+	SDL_Event event;
 	while(running) {
-		SDL_Event event;
 		while(SDL_PollEvent(&event)) {
 			if(event.type == SDL_QUIT) {
 				running = false;
 			}
 		}
+
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
 	}
 
 	SDL_DestroyWindow(window);
