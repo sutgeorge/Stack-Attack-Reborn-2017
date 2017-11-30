69e3940f6446b50ec69989cfa500550841c077fa
George <georgesut@yahoo.com>
2017-11-30 22:45:22 +0200

    Add window

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#define WINDOW_WIDTH 700
+#define WINDOW_HEIGHT 500
 
 int main() {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Stack Attack Reborn",
+										  0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);	
+	if(window == NULL) {
+		printf("Window creation failed!\n");
+		return 0;
+	}
 
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
 }
