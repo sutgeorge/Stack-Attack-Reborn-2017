ecf980227a0db955a49e3c451ddf15126440a83a
George <georgesut@yahoo.com>
2017-11-30 23:05:29 +0200

    Add main loop

--- main.c
+++ main.c
@@ -6,10 +6,21 @@
 int main() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("Stack Attack Reborn",
-										  0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);	
+										  0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+	bool running = true;
+
 	if(window == NULL) {
 		printf("Window creation failed!\n");
 		return 0;
+	}
+
+	while(running) {
+		SDL_Event event;
+		while(SDL_PollEvent(&event)) {
+			if(event.type == SDL_QUIT) {
+				running = false;
+			}
+		}
 	}
 
 	SDL_DestroyWindow(window);
