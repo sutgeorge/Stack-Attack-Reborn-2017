bf0c06c6578f7265d78395c8b952c7a4b3f27f0b
George <georgesut@yahoo.com>
2017-12-01 11:24:05 +0200

    Set default window position to the center of the screen

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#define WINDOW_WIDTH 700
-#define WINDOW_HEIGHT 500
+#define WINDOW_WIDTH 768
+#define WINDOW_HEIGHT 512
 
 int main() {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_DisplayMode dm;
+	SDL_GetCurrentDisplayMode(0, &dm);
 	SDL_Window* window = SDL_CreateWindow("Stack Attack Reborn",
-										  0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+						dm.w / 2 - WINDOW_WIDTH / 2, 
+						dm.h / 2 - WINDOW_HEIGHT / 2, 
+						WINDOW_WIDTH, WINDOW_HEIGHT, 
+						0);
 	
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_SetRenderDrawColor(renderer, 68, 202, 255, 0);
