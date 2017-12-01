c40126db10c088dea8915c9d2ad358aa6e5b6e1c
George <georgesut@yahoo.com>
2017-12-01 20:34:43 +0200

    Create functions for organizing purposes

--- src/main.c
+++ src/main.c
@@ -3,45 +3,68 @@
 #define WINDOW_WIDTH 768
 #define WINDOW_HEIGHT 512
 
-int main() {
+static bool __init(SDL_Window **window, SDL_Renderer **renderer, struct Player *player) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_DisplayMode dm;
 	SDL_GetCurrentDisplayMode(0, &dm);
-	SDL_Window* window = SDL_CreateWindow("Stack Attack Reborn",
+	*window = SDL_CreateWindow("Stack Attack Reborn",
 						dm.w / 2 - WINDOW_WIDTH / 2, 
 						dm.h / 2 - WINDOW_HEIGHT / 2, 
 						WINDOW_WIDTH, WINDOW_HEIGHT, 
 						0);
 	
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 68, 202, 255, 0);
+	if(*window == NULL) {
+		printf("Window creation failed!\n");
+		return false;
+	}
+
+	*renderer = SDL_CreateRenderer(*window, -1, 0);
+	
+	if(*renderer == NULL) {
+		printf("Renderer creation failed!\n");
+		return false;
+	}
+
+	SDL_SetRenderDrawColor(*renderer, 68, 202, 255, 0);
+
+	init_player(player, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 
+			   "res/builder_sprite_sheet.png", 5, 1, *renderer);
+	return true;
+}
+
+static void __render(SDL_Renderer *renderer, struct Player *player) {
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, player->sprite_sheet, 
+					   &player->frame, &player->dstrect);
+	SDL_RenderPresent(renderer);
+}
+
+static void __exit(SDL_Window *window) {
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+int main() {
+	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
+	struct Player player;
+
+	if(!__init(&window, &renderer, &player))
+		return 0;
 
 	bool running = true;
 
-	if(window == NULL) {
-		printf("Window creation failed!\n");
-		return 0;
-	}
-
-	struct Player player;
-	init_player(&player, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 
-			   "res/builder_sprite_sheet.png", 5, 1, renderer);
-
 	SDL_Event event;
-	while(running) {
+	while(running) {		
 		while(SDL_PollEvent(&event)) {
 			if(event.type == SDL_QUIT) {
 				running = false;
 			}
 		}
 
-		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, player.sprite_sheet, 
-					   &player.frame, &player.dstrect);
-		SDL_RenderPresent(renderer);
+		__render(renderer, &player);
 	}
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	__exit(window);
 	return 0;
 }
