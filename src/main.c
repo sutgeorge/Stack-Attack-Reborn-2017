557b4db3ec27e95e099ca963c493c2cc59a2fc19
George <georgesut@yahoo.com>
2019-10-29 19:20:23 +0200

    Add new exit event mapped to the escape key

--- src/main.c
+++ src/main.c
@@ -40,7 +40,7 @@ static void __handle_input(struct Player *player, bool *loop) {
 	SDL_Event event;
 
 	while(SDL_PollEvent(&event)) {
-		if(event.type == SDL_QUIT) {
+		if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) {
 			*loop = false;
 		}
 	}
